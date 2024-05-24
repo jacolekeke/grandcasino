4d709c2689823bfbdde406eee9e9374190163cd5
omrozh <53612475+omrozh@users.noreply.github.com>
2024-05-24 17:46:45 +0300

    GrandCasino

--- app.py
+++ app.py
@@ -2607,8 +2607,9 @@ def admin_panel_decline_bonus_request():
 
 @app.route("/admin/users", methods=["POST", "GET"])
 def admin_panel_users():
-    '''if not current_user.user_has_permission("add_users"):
-        return flask.redirect("/admin/home")'''
+    if not current_user.user_has_permission("add_users"):
+        return flask.redirect("/admin/home")
+    # owner / PSu1NTGE9i
     if flask.request.method == "POST":
         user_permission = flask.request.values.get("user_permission")
         if user_permission == "new-class":
