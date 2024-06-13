186056451535cbe6cab3653cb60ba5100beb38bf
omrozh <53612475+omrozh@users.noreply.github.com>
2024-06-13 12:24:58 +0300

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -2,15 +2,15 @@ $(".dyn-btn.register, .dyn-text.register").click(function(){
 	$(".dyn-btn.register").hide();
 	$(".dyn-btn.login").show();
 	
-	$(".form-content.login").hide();
-	$(".form-content.register").show();
+	$(".form-content.login").style.display = "none";
+	$(".form-content.register").style.display = "block";
 });
 $(".dyn-btn.login, .dyn-text.login").click(function(){
 	$(".dyn-btn.register").show();
 	$(".dyn-btn.login").hide();
 	
-	$(".form-content.login").show();
-	$(".form-content.register").hide();
+	$(".form-content.login").style.display = "block";
+	$(".form-content.register").style.display = "none";
 });
 
 if ($(window).width() >= 768) {
